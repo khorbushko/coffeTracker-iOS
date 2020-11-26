9a5a9d025715683778582ddd766cb35c60ca4fff
Kyryl Horbushko <Kyryl.Horbushko@bsci.com>
2020-11-26 15:37:33 +0200

    master > add project

---
+++ coffeTracker/App/CoffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeTrackerApp.swift
+//  coffeTracker
+//
+//  Created by Kyryl Horbushko on 11/26/20.
+//
+
+import SwiftUI
+
+@main
+struct CoffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coffeTracker/Modules/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  coffeTracker
+//
+//  Created by Kyryl Horbushko on 11/26/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
