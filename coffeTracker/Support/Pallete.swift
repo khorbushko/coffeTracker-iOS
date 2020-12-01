fd2a0a1189d92eef4ed76149ef18553206014bc9
Kyryl Horbushko <Kyryl.Horbushko@bsci.com>
2020-12-01 20:37:59 +0200

    master > correct typos

--- coffeTracker/Support/Pallete.swift
+++ coffeTracker/Support/Pallete.swift
@@ -11,9 +11,9 @@ import SwiftUI
 enum Pallete {
         
     static var black = Color(red: 0, green: 0, blue: 0)
-    static var white = Color(red: 255, green: 255, blue: 255)
-    static var clear = Color(red: 255, green: 255, blue: 255, opacity: 0)
+    static var white = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255)
+    static var clear = Color(red: 255 / 255, green: 255 / 255, blue: 255 / 255, opacity: 0)
     
     static var blue = Color.blue
-    static var utilityGray = Color(red: 60, green: 60, blue: 67)
+    static var utilityGray = Color(red: 60 / 255, green: 60 / 255, blue: 67 / 255)
 }
